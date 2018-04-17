75473afeca9e6de402968fe7f9701071417f0d88
snail <snail1866@gmail.com>
2018-04-17 20:50:05 +0800

    server and client

---
+++ deer-eureka-client/src/test/java/com/deer/www/deer/eureka/client/DeerEurekaClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deer.www.deer.eureka.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeerEurekaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- deer-eureka-server/src/main/java/com/deer/www/deer/eureka/server/DeerEurekaServerApplication.java
+++ deer-eureka-server/src/main/java/com/deer/www/deer/eureka/server/DeerEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.deer.www.deer.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DeerEurekaServerApplication {
 
