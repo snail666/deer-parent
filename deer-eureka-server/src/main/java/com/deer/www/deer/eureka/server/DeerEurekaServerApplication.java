1ba8eebb9d6dcef68ba0621bf5eb13f63d18307a
snail <snail1866@gmail.com>
2018-04-17 20:33:48 +0800

    1111

---
+++ deer-eureka-server/src/main/java/com/deer/www/deer/eureka/server/DeerEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.deer.www.deer.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeerEurekaServerApplication.class, args);
+	}
+}
