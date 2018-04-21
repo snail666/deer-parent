42d6a8afc0f1abc83c110a0f013d072a3192251f
snail <snail1866@gmail.com>
2018-04-21 13:48:54 +0800

    spring config eureka

--- deer-config-server/src/main/java/com/deer/www/deer/config/server/DeerConfigServerApplication.java
+++ deer-config-server/src/main/java/com/deer/www/deer/config/server/DeerConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.deer.www.deer.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class DeerConfigServerApplication {
 
 	public static void main(String[] args) {
